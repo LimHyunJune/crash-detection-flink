0f1fa259199f1124ea23f61ccfaaf02a3022db3e
Imu <48524793+LimHyunJune@users.noreply.github.com>
2024-08-18 15:40:23 +0900

    feat: table to datastream

--- crash-detection-flink/src/main/java/org/example/Main.java
+++ crash-detection-flink/src/main/java/org/example/Main.java
@@ -1,10 +1,15 @@
 package org.example;
 
 
+import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.table.api.EnvironmentSettings;
+import org.apache.flink.table.api.Table;
 import org.apache.flink.table.api.TableEnvironment;
 import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
+import org.apache.flink.types.Row;
+import org.example.dto.Accelerometer;
 
 public class Main {
     static final String bootsrapServer = "localhost:9094";
@@ -12,12 +17,12 @@ public class Main {
     static final String schemaRegistry = "http://localhost:8081";
 
     public static void main(String[] args) throws Exception {
-        EnvironmentSettings settings = EnvironmentSettings.inStreamingMode();
-        TableEnvironment tableEnv = TableEnvironment.create(settings);
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        StreamTableEnvironment tableEnv = StreamTableEnvironment.create(env);
 
         // Kafka Source 테이블 정의
         tableEnv.executeSql(
-                "CREATE TABLE kafka_source_table (" +
+                "CREATE TABLE accelerometer (" +
                         "  duid STRING," +
                         "  amount DOUBLE" +
                         ") WITH (" +
@@ -35,8 +40,21 @@ public class Main {
         );
 
         // 데이터 처리 및 결과 출력
-        tableEnv.executeSql(
-                "SELECT * FROM kafka_source_table"
-        ).print();
+        Table accelerometer = tableEnv.from("accelerometer");
+//        accelerometer.execute().print();
+        DataStream<Row> dataStream = tableEnv.toDataStream(accelerometer);
+        DataStream<String> resultStream = dataStream.map((MapFunction<Row, String>) row -> {
+            // 각 필드의 값을 가져오기
+            String duid = (String) row.getField(0);
+            Double amount = (Double) row.getField(1);
+
+            // 필드 값을 포함한 문자열 반환
+            return "duid: " + duid + ", amount: " + amount;
+        });
+
+        // 결과 출력
+        resultStream.print();
+        // Flink 작업 실행
+        env.execute("DataStream Row Field Example");
     }
 }
